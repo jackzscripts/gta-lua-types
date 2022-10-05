66a99997cd1dfc460ab9daecec1a4fec752c2396
Jackz <jacksonbixby@gmail.com>
2022-10-05 12:15:17 -0500

    add noself

--- stand.d.ts
+++ stand.d.ts
@@ -1,8 +1,10 @@
+/** @noSelf **/
 declare namespace util {
     function yield(ms?: number): void
     function joaat(name: string): Hash
 }
 
+/** @noSelf **/
 declare namespace entities {
     function create_vehicle(hash: Hash, pos: Vector3, heading: number): EntityHandle
 }
