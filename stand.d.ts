853a69b9d32b5756ee4423e4ffb71b54ebec3316
Jackz <jacksonbixby@gmail.com>
2022-10-05 08:41:07 -0500

    Remove exports

--- stand.d.ts
+++ stand.d.ts
@@ -1,10 +1,8 @@
-import { Hash, EntityHandle, Vector3 } from "./natives.js";
-
-export declare namespace util {
+declare namespace util {
     function yield(ms?: number): void
     function joaat(name: string): Hash
 }
 
-export declare namespace entities {
+declare namespace entities {
     function create_vehicle(hash: Hash, pos: Vector3, heading: number): EntityHandle
 }
